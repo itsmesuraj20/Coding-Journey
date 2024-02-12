faa5a1118a34639fd713b71228b2a46ef2b4a90f
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-02-12 23:58:48 +0530

    Reverse LL

--- ReverseLL.cpp
+++ ReverseLL.cpp
@@ -2,5 +2,16 @@ class {
     public:
     ListNode* reversell(ListNode* head){
 
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL)
+        {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr =temp;    
+        }
+        return prev;
     }
 };
